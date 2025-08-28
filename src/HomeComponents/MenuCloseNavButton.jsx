4f5ce02509780d4b2b101644f8176f7c6cc9506c
Muhammed Elsayed <muhammedelsayedradwan5@gmail.com>
2025-08-28 13:14:14 +0300

    change menu direction

--- src/HomeComponents/MenuCloseNavButton.jsx
+++ src/HomeComponents/MenuCloseNavButton.jsx
@@ -5,7 +5,7 @@ const MenuCloseNavButton = ({setNavMenuOpened,navMenuOpened}) => {
 
     return (
         <span
-            className='absolute top-5 right-5 cursor-pointer
+            className='absolute top-5 left-5 cursor-pointer
                     transition duration-250 ease-in-out
                     hover:invert-70
                     xl:hidden
